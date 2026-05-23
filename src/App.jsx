fbe0fa4608c0b0fb0099d6744ffffc490d5ef269
Sowftware <brcgr230@gmail.com>
2026-05-23 07:32:25 +0200

    correction d emessage non paye

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className=' items-center text-center'>Site temporairement indisponible. Pour toute question, <span>Site temporairement indisponible. Pour toute question, veuillez contacter le support technique.</span> le support technique.</h1>
+
+      <h1 className=' justify-center text-center items-center  mt-32'>Site temporairement indisponible. Pour toute question, veuillez contacter le support technique. le support technique.</h1>
     </div>
   )
 }
